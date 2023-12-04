ed98298cf2689d076c0a7480e0509dfc55187f85
AziretGT <esketcum430@gmail.com>
2023-12-05 04:55:58 +0600

    func2

--- program.py
+++ program.py
@@ -17,3 +17,18 @@ def print_player_board(player_ships, player_shots, computer_shots):
         for cell in computer_shots[i]:
             print(cell, end=" ")
         print()
+
+def is_valid_position(board, ship, row, col):
+    rows, cols = len(board), len(board[0])
+
+    for r, c in ship:
+        if not (0 <= row + r < rows and 0 <= col + c < cols):
+            return False
+        if board[row + r][col + c] != 'O':
+            return False
+        for dr in range(-1, 2):
+            for dc in range(-1, 2):
+                if (0 <= row + r + dr < rows and 0 <= col + c + dc < cols) and board[row + r + dr][col + c + dc] != 'O':
+                    return False
+
+    return True
